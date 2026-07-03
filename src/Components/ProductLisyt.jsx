a350501a81ee0760fe3334e15cd26ce5e5f53632
Eng . Seif Eldeen <seifedeen112006@gmail.com>
2026-07-03 08:28:21 +0300

    filter products

--- src/Components/ProductLisyt.jsx
+++ src/Components/ProductLisyt.jsx
@@ -4,22 +4,43 @@ import { useEffect, useState } from "react"
 
 function ProductList() {
     let [products, setProducts] = useState([])
+    let [filterProducts, setFilterProducts] = useState([])
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
             .then(response => response.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data)
+                setFilterProducts(data)
+            });
     }, [])
 
     return (
         <>
             <h2 className="text-center p-3">Our Products</h2>
+            <div className="btns">
+                <button onClick={() => {
+                    setFilterProducts(
+                        products.filter((product) => product.price > 100)
+                    )
+                }} className="btn btn-info">Grater than 100</button>
+                <button onClick={() => {
+                    setFilterProducts(
+                        products.filter((product) => product.price < 100)
+                    )
+                }} className="btn btn-info">Less than 100</button>
+                <button onClick={() => {
+                    setFilterProducts(products)
+                }} className="btn btn-info">All Products</button>
+            </div>
             <div className="cards">
                 <div className="container">
-                    {products.map((product)=> {
-                        return(
+                    {filterProducts.map((product) => {
+                        return (
                             <>
-                            <Product key={product.id} product={product} />
+                                <Product key={product.id}
+                                    product={product}
+                                />
                             </>
                         )
                     })}
